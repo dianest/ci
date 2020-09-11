e8a09f9481a0b4222361937f7c03511ea6120126
dianest <nddgdd@gmail.com>
2020-09-11 20:33:51 +0100

    Added check

--- src/test/java/ru/netology/statistic/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/ru/netology/statistic/StatisticServiceTest.java
@@ -10,7 +10,8 @@ class StatisticServiceTest {
     void findMax() {
         StatisticService service = new StatisticService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;long actual = service.findMax(incomesInBillions);
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
